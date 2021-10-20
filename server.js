d16079adb050787b0e371e00c430f031c1749bc5
Chris Gonsalves <gonsalv_c_993@hotmail.com>
2021-10-20 11:26:27 -0700

    login and ui added

--- server.js
+++ server.js
@@ -2,19 +2,51 @@
 const express = require('express');
 const app = express();
 var mongoose = require('mongoose');
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("./models/users");
 
-// set our port
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(require("express-session")({
+    secret: "Rusty is a dog",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//set our port
 const port = 3000;
 
-// configuration ===========================================
 
-// config files
+// configuration ===================================
+
+//config files
 var db = require('./config/db');
-console.log("connecting--",db);
+console.log("connecting--", db);
 mongoose.connect(db.url); //Mongoose connection created
 
+
 // frontend routes =========================================================
-app.get('/', function(req, res) {res.send('Hello World this is our App!')});
+app.get('/', function(req, res) {res.render('home')});
+
+//login form
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+//Handling user login
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function (req, res) {
+});
 
 // defining route
 app.get('/tproute', function (req, res) {
@@ -38,3 +70,4 @@ app.get('/api/users', function(req, res) {
 // startup our app at http://localhost:3000
 app.listen(port);
 console.log('Example app listening on port 3000!');
+
